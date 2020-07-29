4db9fb034b644b0a6f0a93e3e20a700d81cde16e
Yani Asari <yani.iman1@gmail.com>
2020-07-29 22:39:55 +0000

    Automatically backed up by Learn

--- lib/rescue_princess_peach.rb
+++ lib/rescue_princess_peach.rb
@@ -1,7 +1,7 @@
-def rescue_princess_peach
+def rescue_princess_peach = status
   status = "rescued"
   puts "Hooray! Mario has rescued Princess Peach."
-  puts catch_phrase
+  
 end
 
 ## Comment the below back in to see method scope in action!
