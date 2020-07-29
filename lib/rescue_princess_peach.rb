1b98648bf09dde2fcfe04c070782c4d6bd453c03
Yani Asari <yani.iman1@gmail.com>
2020-07-29 23:25:40 +0000

    Done.

--- lib/rescue_princess_peach.rb
+++ lib/rescue_princess_peach.rb
@@ -1,6 +1,11 @@
+def catch_phrase
+  puts phrase = "It's-a me, Mario!"
+end
+
 def rescue_princess_peach
   status = "rescued"
   puts "Hooray! Mario has rescued Princess Peach."
+  
 end
 
 ## Comment the below back in to see method scope in action!
